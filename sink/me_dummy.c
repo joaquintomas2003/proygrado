8a8b3044feb91e9e7926b7b8e651dd4cada3aa72
joaquin tomas <joaquintomas2003@gmail.com>
2025-09-20 17:16:49 -0300

    --wip-- [skip ci]

--- sink/me_dummy.c
+++ sink/me_dummy.c
@@ -19,27 +19,6 @@ void timer_thread()
   int i;
 
   while (1) {
-    /* Grab the latch value (test-set).  If it's 0xFF we go to the sleep/scan loop. */
-    mem_test_set(&xfer, (__mem40 void *)&latch, sizeof(xfer));
-    while (xfer == 0xFF) {
-      /* Periodic background work: increment the exported counter atomically. */
-      mem_incr32((__mem40 void *)&dummy_counter);
-
-      /* Yield a bit so we don't busy spin (match behavior in your template). */
-      sleep(INCREMENT_MS);
-
-      /* re-check latch value */
-      mem_test_set(&xfer, (__mem40 void *)&latch, sizeof(xfer));
-    }
-
-    /* If latch was cleared (someone triggered us), do a short immediate burst of work. */
-    for (i = 0; i < 8; i++) {
-      mem_incr32((__mem40 void *)&dummy_counter);
-    }
-
-    /* Release the latch (clear the test-set) so others waiting can be notified. */
-    tmp = 0xFF;
-    mem_test_clr(&tmp, (__mem40 void *)&latch, sizeof(tmp));
   }
 }
 
