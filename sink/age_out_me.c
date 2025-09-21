d39f66b85ba89ae7b48311ba3d62294fa94f1706
joaquin tomas <joaquintomas2003@gmail.com>
2025-09-21 19:44:01 -0300

    --wip-- [skip ci]

--- sink/age_out_me.c
+++ sink/age_out_me.c
@@ -1,12 +1,7 @@
 #include <nfp.h>
 #include <stdint.h>
+#include <nfp/me.h>
 #include <nfp/mem_atomic.h>
-#include <nfp_chipres.h>
-#include <pkt/pkt.h>
-#include <pktio/pktio.h>
-#include <fast_meter.h>
-#include <modscript/modscript.h>
-#include <blm.h>
 
 #define FLOWCACHE_ROWS (1 << 18)
 #define BUCKET_SIZE 12
