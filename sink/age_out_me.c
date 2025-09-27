128225199574d096c8467eea51f9c14983bd3f5a
joaquin tomas <joaquintomas2003@gmail.com>
2025-09-27 17:08:07 -0300

    Fix copy to ring logic

--- sink/age_out_me.c
+++ sink/age_out_me.c
@@ -127,31 +127,32 @@ void evict_stale_entries(uint64_t threshold_ns) {
 
                   // Copy key
                   {
-                    __xrw uint32_t key_buf;
-                    mem_read_atomic(&key_buf, (__mem void *)&entry->key, sizeof(key_buf));
-                    mem_write_atomic(&key_buf, (__mem void *)&r_entry->key, sizeof(key_buf));
+                    __xrw uint32_t key[4];
+                    int key_index;
+                    for (key_index = 0; key_index < 4; key_index++) {
+                        key[key_index] = entry->key[key_index];
+                    }
+                    mem_write_atomic(&key, (__mem void *)&r_entry->key, sizeof(key));
                   }
 
                   // Copy packet_count
                   {
                     __xrw uint32_t pkt_cnt_buf;
-                    mem_read_atomic(&pkt_cnt_buf, (__mem void *)&entry->packet_count, sizeof(pkt_cnt_buf));
+                    pkt_cnt_buf = entry->packet_count;
                     mem_write_atomic(&pkt_cnt_buf, (__mem void *)&r_entry->packet_count, sizeof(pkt_cnt_buf));
                   }
 
                   // Copy last_update_timestamp
                   {
                     __xrw uint64_t ts_buf;
-                    mem_read_atomic(&ts_buf, (__mem void *)&entry->last_update_timestamp, sizeof(ts_buf));
+                    ts_buf = entry->last_update_timestamp;
                     mem_write_atomic(&ts_buf, (__mem void *)&r_entry->last_update_timestamp, sizeof(ts_buf));
                   }
 
                   // Copy int_metric_info_value.node_count
                   {
                     __xrw uint32_t node_cnt_buf;
-                    mem_read_atomic(&node_cnt_buf,
-                        (__mem void *)&entry->int_metric_info_value.node_count,
-                        sizeof(node_cnt_buf));
+                    node_cnt_buf = entry->int_metric_info_value.node_count;
                     mem_write_atomic(&node_cnt_buf,
                         (__mem void *)&r_entry->int_metric_info_value.node_count,
                         sizeof(node_cnt_buf));
@@ -163,17 +164,13 @@ void evict_stale_entries(uint64_t threshold_ns) {
                       __xrw int_metric_sample tmp_avg;
 
                       // Copy latest[k]
-                      mem_read_atomic(&tmp_latest,
-                          (__mem void *)&entry->int_metric_info_value.latest[k],
-                          sizeof(tmp_latest));
+                      tmp_latest = entry->int_metric_info_value.latest[k];
                       mem_write_atomic(&tmp_latest,
                           (__mem void *)&r_entry->int_metric_info_value.latest[k],
                           sizeof(tmp_latest));
 
                       // Copy average[k]
-                      mem_read_atomic(&tmp_avg,
-                          (__mem void *)&entry->int_metric_info_value.average[k],
-                          sizeof(tmp_avg));
+                      tmp_avg = entry->int_metric_info_value.average[k];
                       mem_write_atomic(&tmp_avg,
                           (__mem void *)&r_entry->int_metric_info_value.average[k],
                           sizeof(tmp_avg));
