65a65933067b8f175fdbae176963481af26eb44d
JoaquinMezquita <joaquin.mezquita00@gmail.com>
2025-11-19 20:45:47 -0300

    evict

--- sink/main.c
+++ sink/main.c
@@ -208,6 +208,9 @@ int pif_plugin_save_in_hash(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *match_da
         victim_entry = entry;
     }
   }
+evict_flow:
+
+  ts_evict_inicio = me_tsc_read(); 
   timestamp = evict_selected ? aged_ts : min_ts;
   // If we reached the end of the bucket without finding a match
   if (i == BUCKET_SIZE || evict_selected) {
@@ -264,11 +267,16 @@ int pif_plugin_save_in_hash(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *match_da
       ring_meta_read.full          = f;
       ring_meta_read.read_pointer  = rp;
       mem_write_atomic(&ring_meta_read, &ring_G[ring_index], sizeof(ring_meta_read));
+      ts_evict_fin = me_tsc_read();
+      ts_inicio = ticks_to_ns(ts_evict_inicio);
+      ts_fin    = ticks_to_ns(ts_evict_fin);
+      latency = ts_fin - ts_inicio;
+      _save_global_sample(latency);
     semaphore_up(&ring_buffer_sem_G[ring_index]);
+    
   }
 
 save_entry:
-  ts_evict_inicio = me_tsc_read(); 
   // Metadata pointers for nodes
   node_metadata_ptrs[0] = headers + PIF_PARREP_ingress__node1_metadata_OFF_LW;
   node_metadata_ptrs[1] = headers + PIF_PARREP_ingress__node2_metadata_OFF_LW;
@@ -360,11 +368,7 @@ save_entry:
     /* We can write after the IF without problem */
     mem_write_atomic(&sample, &entry->int_metric_info_value.latest[k], sizeof(sample));
   }
-  ts_evict_fin = me_tsc_read();
-  ts_inicio = ticks_to_ns(ts_evict_inicio);
-  ts_fin    = ticks_to_ns(ts_evict_fin);
-  latency = ts_fin - ts_inicio;
-  _save_global_sample(latency);
+
   semaphore_up(&global_semaphores[hash_value]);
 
   /* === End-to-end hop-latency events (T/C) === */
